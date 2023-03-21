cd31ddc4fd92b57df454496fd138dc395ad0da12
Brycies <brysonodhiambo29@gmail.com>
2023-03-21 13:11:34 +0300

    commit app

---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
