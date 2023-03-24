1f2da66cd12eda8ef9dfa2e3fe63929c3e0c992b
Brycies <brysonodhiambo29@gmail.com>
2023-03-24 15:05:44 +0300

    commit app

--- employess.js
+++ employess.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import femaleProfile from "./Images/femaleProfile.jpg";
+import maleProfile from "./Images/maleProfile.jpg";
 
 const Employees = () => {
   const [employees, setEmployees] = useState([
@@ -99,8 +101,31 @@ const Employees = () => {
     },
   ]);
   return (
-    <main>
-      <h1></h1>
+    <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <div class="card-collection">
+            {employees &&
+              employees.map((employee) => (
+                <div
+                  id={employee.id}
+                  classNAme="card m-2"
+                  style={{ cursor: "pointer" }}
+                >
+                  <img src={femaleProfile} classNAme="card-img-top" />
+                  <div classNaMe="card-body">
+                    <h5 className="card-title ">
+                      Full Name {employee.fullName}{" "}
+                    </h5>
+                    <p className="card-text">
+                      <b>Designation</b> {employees.designation}
+                    </p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
